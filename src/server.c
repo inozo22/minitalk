4ebbf35a4c8790d7b463cc50978b1d9f2e25deb2
Nozomi <119419194+inozo22@users.noreply.github.com>
2023-04-04 15:34:32 +0000

    .

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 17:54:56 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/04 07:50:30 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/04 14:22:41 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	send_char(pid_t pid, char c)
 //youcheck
 //		usleep(50);
 		bit = (uc >> i) & 0x01;
-		if (kill(pid, SIGUSER1 + bit) == -1)
+		if (kill(pid, SIGUSR1 + bit) == -1)
 			fatal("kill error");
 	}
 }
@@ -37,10 +37,10 @@ void    receiver(void handler(int, siginfo_t *, void *))
 	ft_bzero(&sa, sizeof(struct sigaction));
 	sa.sa_sigaction = handler;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flag = SA_SIGINFO;//to pass pid, uid and info in siginfo_t
+	sa.sa_flags = SA_SIGINFO;//to pass pid, uid and info in siginfo_t
     //if doesn't use this above, only I can pass is signal number
-	sigaction(SIGUSER1, &sa, NULL);
-	sigaction(SIGUSER2, &sa, NULL);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 }
 
 int main(void)
