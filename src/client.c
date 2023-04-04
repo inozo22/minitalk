91b4e68de8b32b09230ae6224bfc1e597a21b244
Nozomi <119419194+inozo22@users.noreply.github.com>
2023-04-04 15:53:47 +0000

    start communicate but I got segfault

--- src/client.c
+++ src/client.c
@@ -6,14 +6,12 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 17:33:59 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/02 16:17:54 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/04 15:52:47 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 //Retention of header files needs to be considered.
-
-
 
 void	send_char(pid_t srv_pid, char c)
 {
@@ -26,17 +24,17 @@ void	send_char(pid_t srv_pid, char c)
 	while (++i < 8)
 	{
 //youcheck
-//		usleep(50);
+		usleep(50);
 		bit = (uc >> i) & 0x01;
 		if (kill(srv_pid, SIGUSR1 + bit) == -1)
-			return (0);//
+			exit (0);//
 	}
 }
 
 static void	send_str(pid_t srv_pid, char *str)
 {
 	if (ft_strlen(str) > BUFFER_SIZE - 1)
-		return (0);//
+		exit (0);//
 	while (*str)
 	{
 		send_char(srv_pid, *str++);
@@ -49,7 +47,7 @@ static void	client_action(int sig, siginfo_t *info, void *context)
 	(void)context;
 	(void)info;
 	if (sig == SIGUSR2)
-		return (0);//
+		exit (0);//
 	exit(0);//
 }
 
@@ -58,9 +56,9 @@ void   receiver(void action(int, siginfo_t *, void *))
 	struct sigaction    sa;
 
 	ft_bzero(&sa, sizeof(struct sigaction));
-	sa.sa_sigaction = &action;
+	sa.sa_sigaction = &client_action;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flag = SA_SIGINFO;
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 }
@@ -68,11 +66,9 @@ void   receiver(void action(int, siginfo_t *, void *))
 int	main(int ac, char **av)//accept server PID and str to send as arguments
 {
 	pid_t	srv_pid;
-	//somosomo, after execute the server, I execute client by my self?
+
 	if (ac != 3 || !ft_strlen(av[2]))//I think PID it's necessary?
 		return (0);
-	//I have to send the str to server.
-	//Only I can communicate between client and server throw the UNIX signal
 	errno = 0;
 	srv_pid = (pid_t)ft_atoi(av[1]);
 	receiver(client_action);
--- src/server.c
+++ src/server.c
@@ -6,28 +6,42 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 17:54:56 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/04 14:22:41 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/04 15:51:51 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
+static int	sent_str(unsigned char *buf, int index, siginfo_t *info)
+{
+	ft_putendl_fd((char *)buf, 1);
+	ft_bzero(buf, index);
+	kill(info->si_pid, SIGUSR2);
+	return (0);	
+}
+
 static void	server_action(int sig, siginfo_t *info, void *context)
 {
+	static unsigned char	buf[BUFFER_SIZE];
+	static int				index;
+	static int				i;
+	static unsigned char 	uc;
 
-<<<<<<< HEAD
-=======
-	uc = (unsigned char)c;
-	i = -1;
-	while (++i < 8)
+	(void)context;
+	if (sig == SIGUSR2)
+		uc |= (1 << i);
+	i++;
+	if (i == 8)
 	{
-//youcheck
-//		usleep(50);
-		bit = (uc >> i) & 0x01;
-		if (kill(pid, SIGUSR1 + bit) == -1)
-			fatal("kill error");
+		if (index > BUFFER_SIZE - 1)
+			;
+		if (uc == 0)
+			index = sent_str(buf, index, info);
+		else
+			buf[index++] = uc;
+		i = 0;
+		uc = 0;
 	}
->>>>>>> 4ebbf35a4c8790d7b463cc50978b1d9f2e25deb2
 }
 
 void   receiver(void action(int, siginfo_t *, void *))
@@ -35,38 +49,23 @@ void   receiver(void action(int, siginfo_t *, void *))
 	struct sigaction    sa;
 
 	ft_bzero(&sa, sizeof(struct sigaction));
-	sa.sa_sigaction = &action;
+	sa.sa_sigaction = &server_action;
 	sigemptyset(&sa.sa_mask);
-<<<<<<< HEAD
-	sa.sa_flag = SA_SIGINFO;
-=======
-	sa.sa_flags = SA_SIGINFO;//to pass pid, uid and info in siginfo_t
-    //if doesn't use this above, only I can pass is signal number
->>>>>>> 4ebbf35a4c8790d7b463cc50978b1d9f2e25deb2
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 }
 
 int main(void)
 {
-
     pid_t   pid;
 
-
-//output pid//
     ft_putstr_fd("pid: ", 1);
     pid = getpid();
     ft_putnbr_fd(pid, 1);
     ft_putchar_fd('\n', 1);
-//keep running this process
     receiver(server_action);
- 
     while (1)
         pause ();
-//when it receives the string, it has to print it (as soon as possible as it can)
-
-
-//After the first string, also it has to be able to accept strings without restart
-
     return (0);
 }
