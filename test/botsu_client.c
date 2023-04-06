26c4d7d484a2f8b9d235926befcb8db6d830c393
nimai <119419194+inozo22@users.noreply.github.com>
2023-04-06 13:48:02 +0200

    gigiari

--- test/botsu_client.c
+++ test/botsu_client.c
@@ -6,14 +6,16 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 17:33:59 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/06 11:51:47 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/06 13:36:43 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 //Retention of header files needs to be considered.
 
-static void	send_char(pid_t srv_pid, char c)
+sig_atomic_t	volatile	flag;
+
+void	send_char(pid_t srv_pid, char c)
 {
 	int				bit;
 	int				i;
@@ -24,31 +26,44 @@ static void	send_char(pid_t srv_pid, char c)
 	while (++i < 8)
 	{
 //youcheck
-		usleep(50);
-		bit = (uc >> i) & 0x01;
+//		usleep(50);
+		flag = 0;
+		if (uc & 1)
+			kill(srv_pid, SIGUSR1);
+		else
+			kill(srv_pid, SIGUSR2);
+		i++;
+		uc = uc >> 1;
+		while (!flag)
+			pause();
+/* 		bit = (uc >> i) & 0x01;
 		if (kill(srv_pid, SIGUSR1 + bit) == -1)
-			_exit (0);
+			_exit (0); */
 	}
+	flag = 0;
 }
 
-static void	send_str(pid_t srv_pid, char *str)
+void	send_str(pid_t srv_pid, char *str)
 {
 /* 	if (ft_strlen(str) > BUF_SIZE - 1)
 		_exit (0); */
 	while (*str)
 	{
-		send_char(srv_pid, *str++);
+		send_char(srv_pid, *str);
+		str++;
 	}
 	send_char(srv_pid, '\0');
 }
 
-static void	client_action(int sig, siginfo_t *info, void *context)
+void	client_action(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
 	(void)info;
+	if (sig == SIGUSR1)
+		flag = 1;
 	if (sig == SIGUSR2)
 		ft_putendl_fd("Successfull\n", 1);
-	_exit (0);
+//	_exit (0);
 }
 
 void	receiver(void action(int, siginfo_t *, void *))
@@ -59,8 +74,10 @@ void	receiver(void action(int, siginfo_t *, void *))
 	sa.sa_sigaction = &client_action;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
+	sigaddset(&sa.sa_mask, SIGUSR1);
+//	sigaddset(&sa.sa_mask, SIGUSR2);
 	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+//	sigaction(SIGUSR2, &sa, NULL);
 }
 
 int	main(int ac, char **av)//accept server PID and str to send as arguments
@@ -82,5 +99,5 @@ int	main(int ac, char **av)//accept server PID and str to send as arguments
 		return (0);
 	receiver(client_action);
 	send_str(srv_pid, av[2]);
-	return (0);
+//	return (0);
 }
--- test/botsu_server.c
+++ test/botsu_server.c
@@ -6,13 +6,13 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 17:54:56 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/06 12:17:39 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/06 13:39:40 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-static int	put_str(unsigned char *buf, int index, siginfo_t *info)
+/* static int	put_str(unsigned char *buf, int index, siginfo_t *info)
 {
 //	printf("Line: %d, index: %d\n", __LINE__, index);
 	ft_putendl_fd((char *)buf, 1);
@@ -20,30 +20,64 @@ static int	put_str(unsigned char *buf, int index, siginfo_t *info)
 	kill(info->si_pid, SIGUSR2);
 //	printf("Line: %d, info->si_pid: %d\n", __LINE__, info->si_pid);
 	return (0);
+} */
+
+void	char_printer(int bit, int c_pid)
+{
+	static int i;
+	static unsigned char uc;
+
+	i = 0;
+	uc = 0;
+	if (i <= 7)
+	{
+		uc |= (bit << i);
+		i++;
+		kill(c_pid, SIGUSR1);
+	}
+	if (i > 7)
+	{
+		if (uc)
+			write (1, &uc, 1);
+		uc = 0;
+		i = 0;
+	}
 }
 
-static void	server_action(int sig, siginfo_t *info, void *context)
+/* static void	server_action(int sig, siginfo_t *info, void *context)
 {
-	static unsigned char	buf[BUF_SIZE];
-	static int				index;
+//	static unsigned char	buf[BUF_SIZE];
+//	static int				index;
 	static int				i;
 	static unsigned char	uc;
 
 	(void)context;
 	if (sig == SIGUSR2)
+	{
 		uc |= (1 << i);
+		kill(info->si_pid, SIGUSR1);
+	}
 	i++;
 	if (i == 8)
 	{
-/* 		if (index > BUF_SIZE - 1)
-			_exit (0); */
+		if (index > BUF_SIZE - 1)
+			_exit (0);
 		if (uc == 0)
 			index = put_str(buf, index, info);
 		else
 			buf[index++] = uc;
+		if (uc)
+			write(1, &uc, 1);
 		i = 0;
 		uc = 0;
 	}
+} */
+
+
+void	server_action(int sig, siginfo_t *info, void *context)
+{
+	(void)context;
+	char_printer(sig == SIGUSR1, info->si_pid);
 }
 
 void	receiver(void action(int, siginfo_t *, void *))
@@ -54,6 +88,8 @@ void	receiver(void action(int, siginfo_t *, void *))
 	sa.sa_sigaction = &server_action;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 }
@@ -69,5 +105,5 @@ int	main(void)
 	receiver(server_action);
 	while (1)
 		pause ();
-	return (0);
+//	return (0);
 }
